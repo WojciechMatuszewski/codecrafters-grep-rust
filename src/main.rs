2324455619be9fd700846937eab2cb5cb3e4eeb2
Wojciech Matuszewski <wm.matuszewski@gmail.com>
2023-02-28 05:20:05 +0100

    match a literal character

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+
 use std::io;
 use std::process;
 
@@ -12,23 +13,59 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
 
 // Usage: echo <input_text> | your_grep.sh -E <pattern>
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     if env::args().nth(1).unwrap() != "-E" {
         println!("Expected first argument to be '-E'");
         process::exit(1);
     }
 
     let pattern = env::args().nth(2).unwrap();
+    println!("pattern: {:?}", pattern);
     let mut input_line = String::new();
 
     io::stdin().read_line(&mut input_line).unwrap();
 
-    // Uncomment this block to pass the first stage
-    // if match_pattern(&input_line, &pattern) {
-    //     process::exit(0)
-    // } else {
-    //     process::exit(1)
-    // }
+    if match_pattern(&input_line, &pattern) {
+        process::exit(0)
+    } else {
+        process::exit(1)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use assert_cmd::Command;
+    use std::error::Error;
+
+    #[test]
+    fn validates_the_first_parameter() -> Result<(), Box<dyn Error>> {
+        let mut cmd = Command::cargo_bin("grep-starter-rust")?;
+        cmd.arg("a").write_stdin("apple").assert().failure().code(1);
+
+        return Ok(());
+    }
+
+    #[test]
+    fn single_character() -> Result<(), Box<dyn Error>> {
+        {
+            let mut cmd = Command::cargo_bin("grep-starter-rust")?;
+            cmd.arg("-E")
+                .arg("a")
+                .write_stdin("apple")
+                .assert()
+                .success()
+                .code(0);
+        }
+
+        {
+            let mut cmd = Command::cargo_bin("grep-starter-rust")?;
+            cmd.arg("-E")
+                .arg("d")
+                .write_stdin("apple")
+                .assert()
+                .failure()
+                .code(1);
+        }
+
+        return Ok(());
+    }
 }
